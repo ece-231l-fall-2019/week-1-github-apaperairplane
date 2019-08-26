fa26cb54213e2abfcfc8c0a8e0cbe7703c3c9f6a
Bryan Carrillo <apaperairplane@unm.edu>
2019-08-26 13:27:14 -0600

    first commit

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "# **Bryan Carrillo**" << std::endl;
+	std::cout << "I am a student at UNM. Majoring in computer engineering." << std::endl 
+			"I play the guitar and I live for music and art." <<std::endl
+			"I listen to a lot of metal and rock, and a *bit* of everything else." <<std::endl;
+	std::cout << "## I don't really like to talk about myself so I'll end it with some random information." << std::endl
+			"* I have never been on a plane." << std::endl
+			"* My brain is being cooked this year by calculus 2 and physics." << std::endl
+			"* I am working on my own album (but haven't had time :/ )" << std::endl
+			"* I have a small dog named Maya and I love her." << std::endl
+			"* I *used* to work with my film friends as an editor and cameraman." << std::endl
+			"* I am also programming my own text adventure game. In development for 3 years." << std::endl;
+	std::cout << "That is me in a nutshell." << std::endl;
+
+	return 0;
+}
